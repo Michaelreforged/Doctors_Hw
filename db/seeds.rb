30f10c4b8694d859afbf0e1f743473b731442acb
Michael Cheung <michaelcheung.cheme@gmail.com>
2021-09-15 15:54:18 -0600

    added routing, controller, seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+Appointment.destroy_all
+Doctor.destroy_all
+User.destroy_all
+
+doctors = Doctor.create([{name:"Dr. Thomas Wayne"},
+{name:"Dr. Jane Doe"},
+{name:"Dr. Stephen Strange"}])
+
+3.times do |i|
+  user = User.create(name: Faker::Name.name)
+  Appointment.create(date: Faker::Time.forward(days: 30, period: :afternoon), desc: "Regular Check up" ,user_id: user.id, doctor_id: doctors[0].id)
+  Appointment.create(date: Faker::Time.forward(days: 30, period: :morning), desc: "Dental Check up" ,user_id: user.id, doctor_id: doctors[1].id)
+  Appointment.create(date: Faker::Time.forward(days: 30, period: :evening), desc: "Eye Perscription" ,user_id: user.id, doctor_id: doctors[2].id)
+end
+
+puts "Appointment size: #{Appointment.all.length}"
+puts "User size: #{User.all.length}"
+puts "Doctor size: #{Doctor.all.length}"
